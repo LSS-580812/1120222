64ab557a925edf805c436ef794096c57a14f08b7
LSS-580812 <lss.cityhunter@gmail.com>
2023-02-22 18:07:52 +0800

    加入專案檔案。

---
+++ dbForm/Models/ContactsModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+
+namespace dbForm.Models
+{
+    public partial class ContactsModel : DbContext
+    {
+        public ContactsModel()
+            : base("name=ContactsModel")
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", Directory.GetCurrentDirectory());
+        }
+
+        public virtual DbSet<ContactsTable> ContactsTable { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+
+    
+}
---
+++ dbForm/ViewForm.cs
@@ -0,0 +1,28 @@
+﻿using dbForm.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dbForm
+{
+    public partial class ViewForm : Form
+    {
+        public ViewForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BindData()
+        {
+            var context = new ContactsModel();
+            var list = context.ContactsTable.ToList();
+            dataGridView1.DataSource = list;
+        }
+    }
+}
